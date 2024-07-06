f611882d52a8384ede6c0433035befce81764c49
Chinmay <Chinmay.ons@gmail.com>
2024-07-06 13:56:20 +0530

    Cores modified in serverjs

--- server.js
+++ server.js
@@ -5,8 +5,22 @@ import bookRouter from './routes/bookRoute.js';
 import userRouter from './routes/userRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
+import dotenv from 'dotenv';
 
-import 'dotenv/config';  //--dotenv
+dotenv.config() //--dotenv
+
+
+var whitelist = ['https://bookstore-frontend-y2d7.onrender.com', 'http://localhost:5173', 'http://localhost:5174']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 
 //--app config
 const app = express();
@@ -14,10 +28,9 @@ const PORT = process.env.PORT || 4892;
 
 //--middleware
 app.use(express.json());  
-app.use(cors());
+app.use(cors(corsOptions));
 
-//--DataBase connection--
-connectDB();
+
 
 //--api endpoints--
 app.use('/api/book', bookRouter);
@@ -29,7 +42,11 @@ app.use('/api/order', orderRouter);
 app.get('/', (req, res) => {
     res.send('Book Store for Skill Academy!')
 });
+//--DataBase connection--
+connectDB()
+.then(()=>{
+    app.listen(PORT, () => {
+        console.log(`Server started on http://localhost:${PORT}`) 
+    });
+})
 
-app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`) 
-});
